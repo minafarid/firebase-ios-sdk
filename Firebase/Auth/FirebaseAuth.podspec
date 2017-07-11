minafarid/firebase-ios-sdk

commit 6d17c0718efc0582f4607d40d54a7486ec65e05a

Use modulemap to fix exposure of FirebaseCore's internal headers (#121)

FirebaseCommunity.podspec
@@ -19,16 +19,17 @@ Firebase Development CocoaPod including experimental and community supported fea
   s.osx.deployment_target = '10.10'
   s.default_subspec  = 'Root'
   s.preserve_paths = 'README.md'
+  s.module_map = 'Firebase/Firebase/module.modulemap'
 
   s.subspec 'Root' do |sp|
-    sp.source_files = 'Firebase/Firebase/Firebase.h'
-    sp.public_header_files = 'Firebase/Firebase/Firebase.h'
-    sp.user_target_xcconfig = { 'HEADER_SEARCH_PATHS' => '$(inherited) "${PODS_ROOT}/Firebase/Firebase/Firebase"' }
+    sp.source_files = 'Firebase/Firebase/FirebaseCommunity.h'
+    sp.public_header_files = 'Firebase/Firebase/FirebaseCommunity.h'
   end
 
   s.subspec 'Core' do |sp|
     sp.source_files = 'Firebase/Core/**/*.[mh]'
-    sp.public_header_files = 'Firebase/Core/Public/*.h'
+    sp.public_header_files = 'Firebase/Core/Public/*.h','Firebase/Core/Private/*.h',
+    sp.private_header_files = 'Firebase/Core/Private/*.h'
     sp.dependency 'GoogleToolboxForMac/NSData+zlib', '~> 2.1'
     sp.dependency 'FirebaseCommunity/Root'
   end

AuthSamples/Sample/ApplicationDelegate.m
@@ -17,9 +17,9 @@
 #import "ApplicationDelegate.h"
 
 #import "AuthProviders.h"
-#import "FIRApp.h"
+#import "FirebaseCommunity/FIRApp.h"
 #import "FirebaseAuth.h"
-#import "FIRLogger.h"
+#import "FirebaseCommunity/FIRLogger.h"
 #import "GTMSessionFetcherLogging.h"
 #import "MainViewController.h"
 

AuthSamples/Sample/MainViewController.m
@@ -19,8 +19,9 @@
 #import <objc/runtime.h>
 
 #import "FIRAdditionalUserInfo.h"
-#import "FIRApp.h"
-#import "FIRAppAssociationRegistration.h"
+#import "FirebaseCommunity/FIRApp.h"
+#import "FirebaseCommunity/FIRAppInternal.h"
+#import "FirebaseCommunity/FIRAppAssociationRegistration.h"
 #import "FIROAuthProvider.h"
 #import "FIRPhoneAuthCredential.h"
 #import "FIRPhoneAuthProvider.h"
@@ -486,18 +487,6 @@ typedef void (^ShowEmailPasswordDialogCompletion)(FIRAuthCredential *credential)
     @brief The type of block which gets called when a token is ready.
  */
 typedef void (^FIRTokenCallback)(NSString *_Nullable token, NSError *_Nullable error);
-
-/** @brief Method declarations copied from FIRAppInternal.h to avoid importing non-public headers.
- */
-@interface FIRApp (Internal)
-/** @fn getTokenForcingRefresh:withCallback:
-    @brief Retrieves the Firebase authentication token, possibly refreshing it.
-    @param forceRefresh Forces a token refresh. Useful if the token becomes invalid for some reason
-        other than an expiration.
-    @param callback The block to invoke when the token is available.
- */
-- (void)getTokenForcingRefresh:(BOOL)forceRefresh withCallback:(nonnull FIRTokenCallback)callback;
-@end
 
 /** @category FIRAppAssociationRegistration(Deregistration)
     @brief The category for the deregistration method.

AuthSamples/Samples.xcodeproj/project.pbxproj
@@ -1464,7 +1464,7 @@
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SWIFT_OPTIMIZATION_LEVEL = "-Onone";
 				SWIFT_VERSION = 3.0;
-				USER_HEADER_SEARCH_PATHS = "$(inherited) ../Firebase/Core/Private ../Firebase/Auth/Source";
+				USER_HEADER_SEARCH_PATHS = "$(inherited) ../Firebase/Auth/Source";
 			};
 			name = Debug;
 		};
@@ -1481,7 +1481,7 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.google.FirebaseExperimental1.dev;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SWIFT_VERSION = 3.0;
-				USER_HEADER_SEARCH_PATHS = "$(inherited) ../Firebase/Core/Private ../Firebase/Auth/Source";
+				USER_HEADER_SEARCH_PATHS = "$(inherited) ../Firebase/Auth/Source";
 			};
 			name = Release;
 		};
@@ -1583,7 +1583,6 @@
 					"\"$(SRCROOT)/../Firebase/Auth/Source/RPCs\"",
 					"\"$(SRCROOT)/../Firebase/Auth/Source\"",
 					"\"$(SRCROOT)/../Firebase/Auth/Source/AuthProviders\"",
-					"\"$(SRCROOT)/../Firebase/Core/Private\"",
 				);
 				INFOPLIST_FILE = "$(SRCROOT)/../Example/Auth/Tests/Tests-Info.plist";
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks @loader_path/Frameworks";
@@ -1604,7 +1603,6 @@
 					"\"$(SRCROOT)/../Firebase/Auth/Source/RPCs\"",
 					"\"$(SRCROOT)/../Firebase/Auth/Source\"",
 					"\"$(SRCROOT)/../Firebase/Auth/Source/AuthProviders\"",
-					"\"$(SRCROOT)/../Firebase/Core/Private\"",
 				);
 				INFOPLIST_FILE = "$(SRCROOT)/../Example/Auth/Tests/Tests-Info.plist";
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks @loader_path/Frameworks";

Example/Auth/Tests/FIRApp+FIRAuthUnitTests.h
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#import "FIRAppInternal.h"
+#import "FirebaseCommunity/FIRAppInternal.h"
 
 /** @category FIRApp (FIRAuthUnitTests)
     @brief Tests for @c FIRAuth.

Example/Auth/Tests/FIRApp+FIRAuthUnitTests.m
@@ -16,7 +16,7 @@
 
 #import "FIRApp+FIRAuthUnitTests.h"
 
-#import "FIROptionsInternal.h"
+#import "FirebaseCommunity/FIROptionsInternal.h"
 
 @implementation FIRApp (FIRAuthUnitTests)
 

Example/Auth/Tests/FIRAuthTests.m
@@ -18,11 +18,10 @@
 
 #import <XCTest/XCTest.h>
 
-#import "FIRAppInternal.h"
-#import "FIREmailAuthProvider.h"
-#import "FIRGoogleAuthProvider.h"
-#import "FIRPhoneAuthCredential.h"
-#import "FIRAdditionalUserInfo.h"
+#import "FirebaseCommunity/FIRAppInternal.h"
+#import "FirebaseCommunity/FIREmailAuthProvider.h"
+#import "FirebaseCommunity/FIRGoogleAuthProvider.h"
+#import "FirebaseCommunity/FIRAdditionalUserInfo.h"
 #import "FIRAuth_Internal.h"
 #import "FIRAuthErrorUtils.h"
 #import "FIRAuthDispatcher.h"
@@ -56,6 +55,7 @@
 #import <OCMock/OCMock.h>
 
 #if TARGET_OS_IOS
+#import "FIRPhoneAuthCredential.h"
 #import "FIRPhoneAuthProvider.h"
 #endif
 

Example/Auth/Tests/FIRPhoneAuthProviderTests.m
@@ -16,6 +16,7 @@
 
 #import <XCTest/XCTest.h>
 
+#import "FirebaseCommunity/FIRAuth.h"
 #import "FIRPhoneAuthProvider.h"
 #import "Phone/FIRPhoneAuthCredential_Internal.h"
 #import "Phone/NSString+FIRAuth.h"

commit 25ed6d7fd8c69b96e2b4649c26b8e09d9f8a4b0e

Fix macOS pod spec lint warning regression (#133)
Fix travis error introduced by #121

Firebase/Auth/FirebaseAuth.podspec
@@ -30,6 +30,7 @@ Simplify your iOS development, grow your user base, and monetize more effectivel
     'Source/**/FIRAuthAPNSTokenManager.[mh]',
     'Source/**/FIRAuthAPNSTokenType.[mh]',
     'Source/**/FIRAuthAPNSToken.[mh]',
+    'Source/**/FIRPhoneAuthCredential.[mh]',
     'Source/**/FIRPhoneAuthProvider.[mh]'
   s.public_header_files = 'Source/Public/*.h'
   s.preserve_paths =

FirebaseCommunity.podspec
@@ -43,6 +43,7 @@ Firebase Development CocoaPod including experimental and community supported fea
       'Firebase/Auth/Source/**/FIRAuthAPNSTokenManager.[mh]',
       'Firebase/Auth/Source/**/FIRAuthAPNSTokenType.[mh]',
       'Firebase/Auth/Source/**/FIRAuthAPNSToken.[mh]',
+      'Firebase/Auth/Source/**/FIRPhoneAuthCredential.[mh]',
       'Firebase/Auth/Source/**/FIRPhoneAuthProvider.[mh]'
     sp.public_header_files = 'Firebase/Auth/Source/Public/*.h'
     sp.preserve_paths =